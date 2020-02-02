e264cc7c9509afde7068a1710ef835d68363d851
bensku <bmi@iki.fi>
2020-02-02 23:45:26 +0200

    Stub skripty-runtime module

---
+++ skripty-runtime/src/main/java/io/github/bensku/skripty/runtime/ScriptCompiler.java
@@ -0,0 +1,10 @@
+package io.github.bensku.skripty.runtime;
+
+/**
+ * Compiles scripts into intermediate form that can be interpreted or compiled
+ * to a lower level format.
+ *
+ */
+public class ScriptCompiler {
+
+}
