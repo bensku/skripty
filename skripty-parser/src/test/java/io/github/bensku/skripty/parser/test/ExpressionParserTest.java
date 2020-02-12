4692181b90e8436fd2392f912f1fb00bfed44d10
bensku <bmi@iki.fi>
2020-02-12 21:40:12 +0200

    Update parser test terminology to be less confusing

--- skripty-parser/src/test/java/io/github/bensku/skripty/parser/test/ExpressionParserTest.java
+++ skripty-parser/src/test/java/io/github/bensku/skripty/parser/test/ExpressionParserTest.java
@@ -23,26 +23,33 @@ import io.github.bensku.skripty.parser.pattern.Pattern;
 @TestInstance(Lifecycle.PER_METHOD) // Clean state for every test, just in case...
 public class ExpressionParserTest {
 	
-	private final SkriptType stringType = SkriptType.create(String.class);
+	private static final SkriptType VOID = SkriptType.VOID;
+	private static final SkriptType TEXT = SkriptType.create(String.class);
 
 	private ExpressionParser parser;
 	
-	private Expression constantHello;
-	private Expression exprSay;
-	private Expression exprShouted;
+	private Expression constantStr;
+	private Expression exprConsume;
+	private Expression exprFirst;
+	private Expression exprSecond;
 	
 	@BeforeEach
 	public void initExpressions() {
 		ExpressionRegistry registry = new ExpressionRegistry();
-		constantHello = registry.makeConstant(stringType, "Hello, world!");
-		exprSay = registry.makeCallable(this)
-				.inputTypes(new InputType(true, stringType))
-				.returnType(stringType)
+		constantStr = registry.makeConstant(TEXT, "Hello, world!");
+		exprConsume = registry.makeCallable(this)
+				.inputTypes(new InputType(true, TEXT))
+				.returnType(VOID)
 				.callTargets()
 				.create();
-		exprShouted = registry.makeCallable(this)
-				.inputTypes(new InputType(true, stringType))
-				.returnType(stringType)
+		exprFirst = registry.makeCallable(this)
+				.inputTypes(new InputType(true, TEXT))
+				.returnType(TEXT)
+				.callTargets()
+				.create();
+		exprSecond = registry.makeCallable(this)
+				.inputTypes(new InputType(true, TEXT))
+				.returnType(TEXT)
 				.callTargets()
 				.create();
 	}
@@ -52,9 +59,10 @@ public class ExpressionParserTest {
 		LiteralParser[] literalParsers = new LiteralParser[] {};
 		
 		ExpressionLayer basicLayer = new ExpressionLayer();
-		basicLayer.register(constantHello, Pattern.create("greeting"));
-		basicLayer.register(exprSay, Pattern.create("say ", 0));
-		basicLayer.register(exprShouted, Pattern.create(0, " shouted"));
+		basicLayer.register(constantStr, Pattern.create("string constant"));
+		basicLayer.register(exprConsume, Pattern.create("consume ", 0));
+		basicLayer.register(exprFirst, Pattern.create("first-expr ", 0));
+		basicLayer.register(exprSecond, Pattern.create(0, " second-expr"));
 		ExpressionLayer[] layers = new ExpressionLayer[] {basicLayer};
 		
 		parser = new ExpressionParser(literalParsers, layers);
@@ -78,29 +86,29 @@ public class ExpressionParserTest {
 	
 	@Test
 	public void simpleConstant() {
-		parseAll("greeting", constantHello);
+		parseAll("string constant", constantStr);
 	}
 	
 	@Test
 	public void simpleCallable() {
-		parseAll("say greeting", exprSay);
+		parseAll("consume string constant", exprConsume);
 	}
 	
 	@Test
-	public void recursiveSay() {
-		parseAll("say say say greeting", exprSay);
+	public void recursiveExprs() {
+		parseAll("first-expr first-expr string constant", exprFirst);
 	}
 	
 	@Test
 	public void reverseSay() {
-		parseAll("greeting shouted", exprShouted);
+		parseAll("string constant second-expr", exprSecond);
 		
 		// Things below are technically ambiguous
 		// Current parser prefers to consume as much characters as possible
 		// If this changes (e.g. by matchPattern() redesign), these WILL need to be updated
 		// say (greeting shouted)
-		parseAll("say greeting shouted", exprSay);
+		parseAll("consume string constant second-expr", exprConsume);
 		// say (greeting shouted shouted)
-		parseAll("say greeting shouted shouted", exprSay);
+		parseAll("consume string constant second-expr second-expr", exprConsume);
 	}
 }
