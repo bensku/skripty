c5e9fa2c5d642b812566d289f8d20332005dd2cc
bensku <bmi@iki.fi>
2020-02-23 16:20:02 +0200

    More stack unit tests

--- skripty-runtime/src/test/java/io/github/bensku/skripty/runtime/test/ScriptStackTest.java
+++ skripty-runtime/src/test/java/io/github/bensku/skripty/runtime/test/ScriptStackTest.java
@@ -1,6 +1,7 @@
 package io.github.bensku.skripty.runtime.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
 
@@ -15,5 +16,25 @@ public class ScriptStackTest {
 		stack.push(o1);
 		assertEquals(o1, stack.peek());
 		assertEquals(o1, stack.pop());
+		
+		for (int i = 0; i < 10; i++) {
+			stack.push(i); // Autoboxing
+		}
+		Object[] poked = stack.peek(10);
+		Object[] popped = stack.pop(10);
+		for (int i = 0; i < 10; i++) {
+			assertEquals(i, poked[i]);
+			assertEquals(i, popped[i]);
+		}
+		
+		for (int i = 0; i < 10; i++) {
+			stack.push(i); // Autoboxing
+		}
+		Object[] out = new Object[11];
+		stack.popInto(out, 1, 10);
+		assertNull(out[0]);
+		for (int i = 0; i < 10; i++) {
+			assertEquals(i, out[i + 1]);
+		}
 	}
 }
